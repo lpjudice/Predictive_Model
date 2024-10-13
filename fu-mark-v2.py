aeeb6a7e54b2c02f47b841822c607ece71a8332d
lucasjudice <lucas@freatz.com>
2024-10-13 15:33:45 -0700

    Update fu-mark-v2.py

--- fu-mark-v2.py
+++ fu-mark-v2.py
@@ -23,6 +23,8 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+
 
 ## PART 1
 
