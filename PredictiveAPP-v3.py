bb44343cc866bb421a8aaeda5cf05ced4f15d1d4
lucasjudice <lucas@freatz.com>
2024-10-20 18:01:23 -0700

    Update PredictiveAPP-v3.py

--- PredictiveAPP-v3.py
+++ PredictiveAPP-v3.py
@@ -23,6 +23,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 from googleapiclient.errors import HttpError
 import pickle
 import json
