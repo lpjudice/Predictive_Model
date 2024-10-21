8f0981be97142a67681eca84d591269b6458a5e8
lucasjudice <lucas@freatz.com>
2024-10-21 02:43:03 -0700

    Update load_files.py

--- load_files.py
+++ load_files.py
@@ -3,7 +3,7 @@ import json
 import base64
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-import streamlit
+import streamlit as st
 
 
 def authenticate_google_drive():
